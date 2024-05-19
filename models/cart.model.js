d8f672248f47c6b584d90f00bdff464ba0fba721
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-05-19 11:55:52 -0700

    made changes to the cart

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,46 +1,4 @@
 import mongoose from 'mongoose';
-import User from './user.model.js';
-import products from './product.model.js';
-
-
-// const cartItemSchema = new Schema({
-//     productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-//     quantity: { type: Number, required: true },
-//     price: { type: Number, required: true }
-//   });
-  
-
-//   const CartItem = mongoose.model('CartItem', cartItemSchema);
-//   const cartSchema = new Schema({
-//     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-//     items: [cartItemSchema],
-//     totalPrice: {
-//       type: Number,
-//       required: true,
-//       default: 0
-//     }
-//   });
-  
-//   cartSchema.methods.calculateTotalPrice = function() {
-//     this.totalPrice = this.items.reduce((total, item) => {
-//       return total + item.quantity * item.price;
-//     }, 0);
-//     return this.totalPrice;
-//   };
-  
-//   const Cart = mongoose.model('Cart', cartSchema);
-    
-
-// export default Cart;
-
-
-
-
-
-
-
-
-
 
 const cartItemSchema = new mongoose.Schema({
     product: {
@@ -62,26 +20,61 @@ const cartSchema = new mongoose.Schema({
         required: true
     },
     cartItems: [cartItemSchema],
-     totalPrice: {
+    totalPrice: {
         type: Number,
         required: true,
-        default: 0
+        default: 0, min: 0
+    },
+    totalQuantity: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0 // Ensure the total quantity is not negative
     }
 });
 
-// Middleware to update the total price before saving
+// // Middleware to update the total price before saving
+// cartSchema.pre('save', async function (next) {
+//     const cart = this;
+//     // Fetch product details for each cart item
+//     const productIds = cart.cartItems.map(item => item.product);
+//     const products = await mongoose.model('Products').find({ '_id': { $in: productIds } });
+
+//     // Calculate the total price
+//     cart.totalPrice = cart.cartItems.reduce((total, item) => {
+//         const product = products.find(p => p._id.equals(item.product));
+//         return total + (product ? item.quantity * product.price : 0);
+//     }, 0);
+
+//     next();
+// });
+
+
+// Middleware to update the total price and total quantity before saving
 cartSchema.pre('save', async function(next) {
     const cart = this;
 
-    // Fetch product details for each cart item
-    const productIds = cart.cartItems.map(item => item.product);
-    const products = await mongoose.model('Products').find({ '_id': { $in: productIds } });
+    // Check if cartItems is modified
+    if (cart.isModified('cartItems')) {
+        // Fetch product details for each cart item
+        const productIds = cart.cartItems.map(item => item.product);
+        const products = await mongoose.model('Products').find({ '_id': { $in: productIds } });
 
-    // Calculate the total price
-    cart.totalPrice = cart.cartItems.reduce((total, item) => {
-        const product = products.find(p => p._id.equals(item.product));
-        return total + (product ? item.quantity * product.price : 0);
-    }, 0);
+        // Calculate the total price and total quantity
+        let totalPrice = 0;
+        let totalQuantity = 0;
+
+        cart.cartItems.forEach(item => {
+            const product = products.find(p => p._id.equals(item.product));
+            if (product) {
+                totalPrice += item.quantity * product.price;
+                totalQuantity += item.quantity;
+            }
+        });
+
+        cart.totalPrice = totalPrice;
+        cart.totalQuantity = totalQuantity;
+    }
 
     next();
 });

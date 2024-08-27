88dcbc2d4916d9a2c1be09ffa3fc68126a3c6465
Anteqs <103751233+ayindequdus22@users.noreply.github.com>
2024-08-27 09:41:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,17 @@ app.use(cookieParser());
 app.use(helmet());
 // CORS Configuration
 
-const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",").map(origin => origin.trim().replace(/\/$/, ""));
-app.use(cors(
-    {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}
-));
+//const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",").map(origin => origin.trim().replace(/\/$/, "")); {
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }
+app.use(cors());
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)

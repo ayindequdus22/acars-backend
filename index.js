bffc3fe1dfb60359da3c98c77f352a519a1c9966
Anteqs <103751233+ayindequdus22@users.noreply.github.com>
2024-08-28 01:41:25 -0700

    added the cors back for deployment

--- index.js
+++ index.js
@@ -21,17 +21,18 @@ app.use(cookieParser());
 app.use(helmet());
 // CORS Configuration
 
-//const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",").map(origin => origin.trim().replace(/\/$/, "")); {
-//     origin: (origin, callback) => {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true
-// }
-app.use(cors("*"));
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",").map(origin => origin.trim().replace(/\/$/, ""));
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
